d35a98a56f404ede25ec78b782baaa5ec4dbb350
Alex Kayser <akctba@gmail.com>
2020-03-04 21:49:20 -0800

    Fetch instructions

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
+var data;
+
 $(function () {
     $('#button-search').click(() => {
         if (!$('#ingredient').val()) {
             let msg = "Hey, com'on... Type something first, ok?";
             messageToUser(msg);
         } else {
+            //add a loading
+            loading(true);
 
             //fetch meals
             var requestOptions = {
@@ -17,18 +21,20 @@ $(function () {
             fetch(url, requestOptions)
                 .then(response => response.text())
                 .then(result => {
-                    result = JSON.parse(result);
-                    console.log(result);
-                    if(result.meals != null) {
-                        //add a loading
-                        loading();
-                        let list = $('<ul class="list-group"></ul>');
+                    data = JSON.parse(result);
+                    //console.log(data);
+                    if(data.meals != null) {
+                        
+                        let list = $('<div class="list-group"></div>');
                         $('#meals').append(list);
 
-                        result.meals.forEach(meal => {
+                        data.meals.forEach((meal, i) => {
                             //Add list
-                            let item = $('<li class="list-group-item"></li>');
-                            item.html(meal.strMeal);
+                            let item = `<a href="#receipe" class="list-group-item list-group-item-action" onclick='selectMeal(${i})'>`;
+                            item += '<div class="d-flex w-100 justify-content-between align-items-center">';
+                            item += `<img class='imgThumb' src='${meal.strMealThumb}/preview' alt='thumb_${meal.idMeal}' />`;
+                            item += `<span class='flex-fill ml-2'>${meal.strMeal}</span>`;
+                            item += '</div></a>';
                             list.append(item);
                         });
                         
@@ -41,6 +47,8 @@ $(function () {
                     messageToUser("Ohho.. Something not good happened. Keep calm!");
                     console.error('error', error);
                 });
+
+            loading(false);
         }
     });
 });
@@ -56,6 +64,69 @@ function messageToUser(msg) {
     }, 5000);
 }
 
-function loading() {
-    $('#meals').html('');
+function loading(onOff) {
+    if (onOff)
+        $('#meals').html('<img id="loadingPan" class="imgLoading" src="./images/frypan-white.gif" alt="loading">');
+    else
+        setTimeout(()=> {$('#loadingPan').fadeOut();}, 1000);
+}
+
+function selectMeal(index) {
+    //clean previous meal
+    $('#receipe').html('');
+
+    let meal = data.meals[index];
+
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+      };
+      
+      fetch("https://www.themealdb.com/api/json/v1/1/lookup.php?i="+meal.idMeal, requestOptions)
+        .then(response => response.text())
+        .then(result => {
+            //console.log(result);
+            let mealDet = JSON.parse(result);
+            mealDet = mealDet.meals[0];
+
+            
+            let prev = '<div class="card receipeFrame">'; 
+            prev += `<h5 class="card-header">${meal.strMeal}</h5>`;
+            prev += `<img class="card-img-top" src='${meal.strMealThumb}' alt='pic_${meal.idMeal}' />`;
+            prev += '<div class="card-body">';
+            prev += `<h5 class="card-title">${meal.strMeal}</h5>`;
+            prev += `<p>Origin: ${mealDet.strArea}</p>`;
+            if (mealDet.strCategory) {
+                prev += `<p>Category: ${mealDet.strCategory}</p>`;
+            }
+
+            //ingedients
+            prev += '<h6 class="card-title">Ingredients</h6>';
+            prev += '<ul class="list-group list-group-flush">';
+            for (let i=1; i<=20; i++) {
+                if (mealDet["strIngredient"+i]) {
+                    prev += `<li class="list-group-item">${mealDet["strIngredient"+i]} - ${mealDet["strMeasure"+i]}</li>`;
+                }
+            }
+
+            prev += '</ul>';
+            prev += '<br/>'
+            
+            prev += '<h6 class="card-title">Instructions</h6>';
+
+            //brake in lines to easy reading
+            prev += `<p>${(mealDet.strInstructions).replace(/\. /g,".<br/><br/>")}</p>`;
+            
+
+            prev += '</div><br/></div><br/>';
+
+            $('#receipe').append(prev);
+
+
+        })
+        .catch(error => console.log('error', error));
+
+
+
+    
 }
